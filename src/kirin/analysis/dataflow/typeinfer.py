bec92591356fe7e526e848d2d9acca607dba028f
Roger-luo <rogerluo.rl18@gmail.com>
2024-10-08 15:29:28 -0400

    fix method infer

--- src/kirin/analysis/dataflow/typeinfer.py
+++ src/kirin/analysis/dataflow/typeinfer.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 from kirin.analysis.dataflow.forward import ForwardDataFlowAnalysis
+from kirin.dialects.py import types
 from kirin.interp import Successor
 from kirin.ir import BottomType, TypeAttribute
 from kirin.ir.nodes.stmt import Statement
@@ -21,4 +22,7 @@ class TypeInference(ForwardDataFlowAnalysis[TypeAttribute, WorkList[Successor]])
 
     def build_signature(self, stmt: Statement, args: tuple):
         """we use value here as signature as they will be types"""
-        return (stmt.__class__, args)
+        return (
+            stmt.__class__,
+            tuple(x.typ if isinstance(x, types.PyConst) else x for x in args),
+        )
--- src/kirin/dialects/func/typeinfer.py
+++ src/kirin/dialects/func/typeinfer.py
@@ -1,9 +1,9 @@
 from kirin.analysis.dataflow.typeinfer import TypeInference
 from kirin.dialects.func.dialect import dialect
 from kirin.dialects.func.interp import Interpreter
-from kirin.dialects.func.stmts import Return
+from kirin.dialects.func.stmts import Call, Return
 from kirin.dialects.py import types
-from kirin.interp import ReturnValue, impl
+from kirin.interp import ResultValue, ReturnValue, impl
 
 
 # NOTE: a lot of the type infer rules are same as the builtin dialect
@@ -17,3 +17,22 @@ class TypeInfer(Interpreter):
             return ReturnValue(types.NoneType)
         else:
             return ReturnValue(*values)
+
+    @impl(Call)
+    def call(self, interp: TypeInference, stmt: Call, values: tuple):
+        # NOTE: support kwargs after Call stmt stores the key names
+        n_total = len(values)
+        if stmt.kwargs.data:
+            kwargs = dict(
+                zip(stmt.kwargs.data, values[n_total - len(stmt.kwargs.data) :])
+            )
+        else:
+            kwargs = None
+
+        # give up on dynamic method calls
+        if not isinstance(values[0], types.PyConst):
+            return ResultValue(types.Any)
+
+        return interp.eval(
+            values[0].data, values[1 : n_total - len(stmt.kwargs.data)], kwargs
+        ).to_result()
