fcc0b5aa13f6b89f544377251447de9d26161b0c
Kai-Hsin Wu <khwu@KHWus-MacBook-Pro.local>
2024-10-29 14:15:04 -0400

    refactor to match recent update on main

--- src/kirin/passes/inline.py
+++ src/kirin/passes/inline.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 
 from kirin import ir
-from kirin.interp import Interpreter
 from kirin.passes import Pass
 from kirin.rewrite import Fixpoint, Walk
 from kirin.rules.cfg_compatify import CFGCompactify
@@ -12,12 +11,9 @@ from kirin.rules.inline import Inline
 @dataclass
 class InlinePass(Pass):
 
-    def __post_init__(self):
-        self.interp = Interpreter(self.dialects)
-
     def unsafe_run(self, mt: ir.Method) -> None:
 
-        Walk(Inline(interp=self.interp, heuristic=lambda x: True)).rewrite(mt.code)
+        Walk(Inline(heuristic=lambda x: True)).rewrite(mt.code)
 
         if (trait := mt.code.get_trait(ir.SSACFGRegion)) is not None:
             compactify = Fixpoint(CFGCompactify(trait.get_graph(mt.callable_region)))
