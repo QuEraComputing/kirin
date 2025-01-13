b1f4cff79db30ae79b627db7106114a5f7569526
Roger-luo <rogerluo.rl18@gmail.com>
2025-01-13 03:18:01 -0500

    doc: add docstring for FromPythonCall trait

--- src/kirin/ir/traits/lowering/call.py
+++ src/kirin/ir/traits/lowering/call.py
@@ -16,6 +16,16 @@ StatementType = TypeVar("StatementType", bound="Statement")
 
 @dataclass(frozen=True)
 class FromPythonCall(PythonLoweringTrait[StatementType, ast.Call]):
+    """Trait for customizing lowering of Python calls to a statement.
+
+    Declared in a statement definition to indicate that the statement can be
+    constructed from a Python call (i.e., a function call `ast.Call` in the
+    Python AST).
+
+    Subclassing this trait allows for customizing the lowering of Python calls
+    to the statement. The `lower` method should be implemented to parse the
+    arguments from the Python call and construct the statement instance.
+    """
 
     def lower(
         self, stmt: type[StatementType], state: "LoweringState", node: ast.Call
