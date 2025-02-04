60e966bfae1f6d1c703da973b89f95122fd79e8c
Roger-luo <rogerluo.rl18@gmail.com>
2025-02-03 21:00:29 -0500

    feat: add discard to DialectGroup

--- test/ir/test_group.py
+++ test/ir/test_group.py
@@ -13,3 +13,14 @@ def test_union():
     target_b = group_a.union(group_c)
     assert target_a.data == group_a.data
     assert target_b.data == group_d.data
+
+
+def test_discard():
+    group_a = DialectGroup([base, cf])
+    group_c = DialectGroup([base, func])
+    group_d = DialectGroup([base, func, cf])
+
+    target_a = group_d.discard(cf)
+    target_b = group_d.discard(func)
+    assert target_a.data == group_c.data
+    assert target_b.data == group_a.data
