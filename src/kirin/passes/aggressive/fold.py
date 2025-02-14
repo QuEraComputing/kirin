4a204f3a782cfbf44044f576521aa99c1e3ec743
Roger-luo <rogerluo.rl18@gmail.com>
2025-02-13 20:44:53 -0500

    rewrite rule for unrolling scf.ifelse

--- src/kirin/passes/aggressive/fold.py
+++ src/kirin/passes/aggressive/fold.py
@@ -1,16 +1,69 @@
 from dataclasses import dataclass
 
 from kirin.passes import Pass
-from kirin.rewrite import aggressive
+from kirin.rewrite import (
+    Walk,
+    Chain,
+    Inline,
+    Fixpoint,
+    WrapConst,
+    Call2Invoke,
+    ConstantFold,
+    CFGCompactify,
+    InlineGetItem,
+    InlineGetField,
+    DeadCodeElimination,
+)
 from kirin.analysis import const
 from kirin.ir.method import Method
 from kirin.rewrite.abc import RewriteResult
 
+# Chain(
+#                 Walk(WrapConst(frame)),
+#                 Walk(Inline(lambda _: True)),
+#                 Walk(ConstantFold()),
+#                 Walk(Call2Invoke()),
+#                 Fixpoint(
+#                     Walk(
+#                         Chain(
+#                             InlineGetItem(),
+#                             InlineGetField(),
+#                             DeadCodeElimination(),
+#                         )
+#                     )
+#                 ),
+#                 Walk(CFGCompactify()),
+#             )
+
 
 @dataclass
 class Fold(Pass):
+    max_iter: int = 10
 
     def unsafe_run(self, mt: Method) -> RewriteResult:
+        result = RewriteResult()
         constprop = const.Propagate(self.dialects)
-        constprop_results, _ = constprop.run_analysis(mt)
-        return aggressive.Fold(constprop_results).rewrite(mt.code)
+        for _ in range(self.max_iter):
+            frame, _ = constprop.run_analysis(mt)
+            result = Walk(WrapConst(frame)).rewrite(mt.code).join(result)
+            rule = Chain(
+                ConstantFold(),
+                Call2Invoke(),
+                InlineGetField(),
+                InlineGetItem(),
+                DeadCodeElimination(),
+            )
+            result = Fixpoint(Walk(rule)).rewrite(mt.code).join(result)
+            result = Walk(Inline(lambda _: True)).rewrite(mt.code).join(result)
+            result = Fixpoint(CFGCompactify()).rewrite(mt.code).join(result)
+            if result.has_done_something is False:
+                return RewriteResult(
+                    has_done_something=True,
+                    terminated=result.terminated,
+                    exceeded_max_iter=result.exceeded_max_iter,
+                )
+            if result.terminated:
+                return result
+        return RewriteResult(
+            has_done_something=True, terminated=False, exceeded_max_iter=True
+        )
