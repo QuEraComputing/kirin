019b586f5a3a593305fbde85ff6cd8a8220d33e6
Phillip Weinberg <weinbe58@gmail.com>
2025-03-13 14:37:47 -0400

    adding lark trait for typical statement

---
+++ src/kirin/ir/traits/lark/stmt.py
@@ -0,0 +1,60 @@
+from kirin import ir
+from kirin.decl import fields
+from kirin.exceptions import LarkLoweringError
+from kirin.ir.nodes.stmt import Statement
+from kirin.parse.grammer import LarkParser
+from kirin.lowering.state import LoweringState
+from kirin.lowering.result import Result as Result
+
+from ..abc import LarkLoweringTrait
+
+
+class FromLark(LarkLoweringTrait):
+    def lark_rule(
+        self,
+        grammer_rules: dict[ir.IRNode | ir.Attribute, str],
+        stmt_type: type[Statement],
+    ) -> str:
+        assert (
+            stmt_type.dialect is not None
+        ), f"Statement {stmt_type} must have a dialect"
+
+        stmt_fields = fields(stmt_type)
+
+        if len(stmt_fields.regions) > 0:
+            raise LarkLoweringError(
+                f"Statement {stmt_type} has regions, which are not supported by FromLark trait. create a custom trait for this statement"
+            )
+
+        if len(stmt_fields.blocks) > 0:
+            raise LarkLoweringError(
+                f"Statement {stmt_type} has blocks, which are not supported by FromLark trait. create a custom trait for this statement"
+            )
+
+        num_results = len(stmt_fields.results)
+
+        stmt_body = f'"{stmt_type.dialect.name}.{stmt_type.name}" '
+        return_match = ", ".join("ssa_identifier" for _ in range(num_results))
+        type_match = ", ".join(' "!" attr' for _ in range(num_results))
+        stmt_args = ", ".join(
+            f'"{arg.name}" "=" ssa_identifier' for arg in stmt_fields.args
+        )
+        attr_args = ", ".join(
+            f'"{name}" "=" {grammer_rules[attr.type]}'
+            for name, attr in stmt_fields.attributes.items()
+        )
+
+        stmt_rule = f'{stmt_body} "(" {stmt_args} ")"'
+
+        if len(attr_args) > 0:
+            stmt_rule = f'{stmt_rule} "{{" {attr_args} "}}"'
+
+        if len(return_match) > 0:
+            stmt_rule = f'"{return_match} "=" {stmt_rule} ":" {type_match}'
+
+        return stmt_rule
+
+    def lower(
+        self, parser: LarkParser, state: LoweringState, stmt: Statement
+    ) -> Result:
+        pass
