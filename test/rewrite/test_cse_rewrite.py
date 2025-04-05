21c50abb4c3d251d160b2b649525c0935c5bbc4d
Roger-luo <rogerluo.rl18@gmail.com>
2025-04-04 23:20:10 -0400

    finish up tests

--- test/rewrite/test_cse_rewrite.py
+++ test/rewrite/test_cse_rewrite.py
@@ -1,20 +1,37 @@
+from kirin import ir, lowering
+from kirin.decl import info, statement
 from kirin.prelude import basic_no_opt
 from kirin.rewrite.cse import CommonSubexpressionElimination
 from kirin.rewrite.walk import Walk
 
-
-@basic_no_opt
-def main():
-    x = 1
-    y = 1
-    z = 1
-    return x + y + z
+dialect = ir.Dialect("test")
 
 
-main.print()
+@statement(dialect=dialect)
+class MultiResult(ir.Statement):
+    traits = frozenset({ir.Pure(), lowering.FromPythonCall()})
+    result_a: ir.ResultValue = info.result()
+    result_b: ir.ResultValue = info.result()
 
-# the bug only really rears its head if there's multiple things in the ._result attribute of the statement
-# otherwise it's perfectly valid to handle what just happened
-result = Walk(CommonSubexpressionElimination()).rewrite(main.code)
 
-main.print()
+dummy_dialect = basic_no_opt.add(dialect)
+
+
+def test_multi_result():
+    @dummy_dialect
+    def duplicated():
+        x, y = MultiResult()  # type: ignore
+        a, b = MultiResult()  # type: ignore
+        return x + a, y + b
+
+    stmt_0 = duplicated.callable_region.blocks[0].stmts.at(0)
+    stmt_1 = duplicated.callable_region.blocks[0].stmts.at(1)
+    assert isinstance(stmt_0, MultiResult)
+    assert isinstance(stmt_1, MultiResult)
+
+    Walk(CommonSubexpressionElimination()).rewrite(duplicated.code)
+
+    stmt_0 = duplicated.callable_region.blocks[0].stmts.at(0)
+    stmt_1 = duplicated.callable_region.blocks[0].stmts.at(1)
+    assert isinstance(stmt_0, MultiResult)
+    assert not isinstance(stmt_1, MultiResult)
