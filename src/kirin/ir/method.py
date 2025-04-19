259ab8bbfe6a8db39acb2ed3a0c7dbf04b41cd88
Roger-luo <rogerluo.rl18@gmail.com>
2025-04-19 00:39:23 -0400

    ok nice it errors

--- src/kirin/ir/method.py
+++ src/kirin/ir/method.py
@@ -98,7 +98,7 @@ class Method(Printable, typing.Generic[Param, RetType]):
             self.inferred,
         )
 
-    def verify(self) -> int:
+    def verify(self) -> None:
         """verify the method body.
 
         This will raise a ValidationError if the method body is not valid.
