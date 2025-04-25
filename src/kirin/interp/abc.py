f0f86fb508894da38ccf8fefb54c2b62a03d77e9
Roger-luo <rogerluo.rl18@gmail.com>
2025-04-25 13:54:47 -0400

    interpreter refactor (part II)

---
+++ src/kirin/interp/abc.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from abc import ABC
+from typing import TYPE_CHECKING, Generic, TypeVar, Union, ClassVar
+from dataclasses import dataclass, field
+
+from kirin import ir
+
+from .frame import FrameABC
+from .state import InterpreterState
+from .table import Signature, BoundedDef
+from .value import Successor, ReturnValue, SpecialValue, StatementResult
+from .exceptions import InterpreterError
+
+ValueType = TypeVar("ValueType")
+FrameType = TypeVar("FrameType", bound=FrameABC)
+
+@dataclass
+class InterpreterABC(ABC, Generic[FrameType, ValueType]):
+    keys: ClassVar[tuple[str, ...]]
+
+    def call(self, node: ir.Statement, *args: ValueType) -> ValueType:
+        from kirin.dialects.func import Invoke
+        self.eval(Invoke(args, callee=node))
+    def eval(self, node: ir.Statement) -> ValueType: ...
