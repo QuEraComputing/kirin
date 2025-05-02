b8ee4e91ef00332fac13f603a77de3d335e94552
Xiu-zhe (Roger) Luo <rogerluo.rl18@gmail.com>
2025-05-02 12:28:31 -0400

    Update src/kirin/analysis/const/prop.py

--- src/kirin/analysis/const/prop.py
+++ src/kirin/analysis/const/prop.py
@@ -99,7 +99,7 @@ class Propagate(ForwardExtra[Frame, Result]):
             # it should return the number of Unknowns equal to each result
             if stmt.has_trait(ir.Pure):
                 return tuple(
-                    [Unknown() for result in stmt.results]
+                    Unknown() for result in stmt.results
                 )  # no implementation but pure
             # not pure, and no implementation, let's say it's not pure
             frame.frame_is_not_pure = True
