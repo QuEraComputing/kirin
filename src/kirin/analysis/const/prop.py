10eb354d00b040f53a4e82d0703b5101dd035919
John Long <johnzl@protonmail.ch>
2025-05-02 11:54:18 -0400

    handle multiple result propogation

--- src/kirin/analysis/const/prop.py
+++ src/kirin/analysis/const/prop.py
@@ -95,8 +95,10 @@ class Propagate(ForwardExtra[Frame, Result]):
                 if types.is_tuple_of(values, Value):
                     return self.try_eval_const_pure(frame, stmt, values)
 
-            if stmt.has_trait(ir.Pure):
-                return (Unknown(),)  # no implementation but pure
+            # If a statement is pure but a method was not found in the registry, 
+            # it should return the number of Unknowns equal to each result
+            if stmt.has_trait(ir.Pure): 
+                return tuple([Unknown() for result in stmt.results])  # no implementation but pure
             # not pure, and no implementation, let's say it's not pure
             frame.frame_is_not_pure = True
             return (Unknown(),)
