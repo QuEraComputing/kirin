739169c120cafe553e542016377e170f0c4b740f
David Plankensteiner <da.plankensteiner@gmail.com>
2025-06-13 08:57:28 +0200

    Recursively check all parents for definitions

--- test/dialects/scf/test_ifelse.py
+++ test/dialects/scf/test_ifelse.py
@@ -65,6 +65,9 @@ def test_if_else_defs():
     main2 = main.similar(kernel)
     Fold(main2.dialects)(main2)
 
+    assert main(0) == 1 == main2(0)
+    assert main(10) == 0 == main2(4)
+
     main2.print()
 
     @kernel
@@ -84,3 +87,26 @@ def test_if_else_defs():
     Fold(main_elif2.dialects)(main_elif2)
 
     main_elif2.print()
+
+    assert main_elif(0) == 3 == main_elif2(0)
+    assert main_elif(-1) == 4 == main_elif2(-1)
+    assert main_elif(5) == 0 == main_elif2(7)
+
+    @kernel
+    def main_nested_if(n: int):
+        x = 0
+
+        if n > 0:
+            if n > 1:
+                if n == 3:
+                    x = 4
+
+        return x
+
+    main_nested_if.print()
+
+    main_nested_if2 = main_nested_if.similar(kernel)
+    Fold(main_nested_if2.dialects)(main_nested_if2)
+
+    assert main_nested_if(3) == 4 == main_nested_if2(3)
+    assert main_nested_if(10) == 0 == main_nested_if2(8)
