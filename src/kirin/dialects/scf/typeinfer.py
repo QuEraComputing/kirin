be06ea2c3fafc6408e764e48923c844bcba224df
David Plankensteiner <da.plankensteiner@gmail.com>
2025-07-24 10:13:46 +0200

    Fix type inference of loop variable

--- src/kirin/dialects/scf/typeinfer.py
+++ src/kirin/dialects/scf/typeinfer.py
@@ -45,10 +45,9 @@ class TypeInfer(absint.Methods):
             frame.worklist.append(interp.Successor(body_block, item, *loop_vars))
             return  # if terminate is Return, there is no result
 
-        with interp_.new_frame(stmt, has_parent_access=True) as body_frame:
-            loop_vars_ = interp_.run_ssacfg_region(
-                body_frame, stmt.body, (iterable,) + loop_vars
-            )
+        with interp_.new_frame(stmt) as body_frame:
+            body_frame.entries.update(frame.entries)
+            loop_vars_ = interp_.run_ssacfg_region(body_frame, stmt.body, args=())
 
         frame.entries.update(body_frame.entries)
         if isinstance(loop_vars_, interp.ReturnValue):
