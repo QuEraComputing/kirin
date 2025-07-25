00938ad4d96db245b92c3bd9e06d232717fdee71
David Plankensteiner <david-pl@users.noreply.github.com>
2025-07-25 16:13:35 +0200

    Fix type inference of loop variable on v0.17 (#449)
    **NOTE**: this is a **backport only fix**, i.e. it's a patch for v0.17
    only (the issue is no longer present on v0.18+).
    
    This is a small fix that causes a lot of trouble downstream. It was
    easier to fix here than adding workarounds in bloqade-circuit. I hope
    that's okay.
    
    As far as I can tell, the only thing we "lose" are a couple of tests
    (which I added here) and it makes future fixes that touch the same code
    more difficult to backport (but it's only three lines I touched).

--- test/dialects/scf/test_typeinfer.py
+++ test/dialects/scf/test_typeinfer.py
@@ -1,6 +1,7 @@
 from kirin import types
 from kirin.prelude import structural_no_opt
 from kirin.analysis import TypeInference
+from kirin.dialects import ilist
 
 type_infer = TypeInference(structural_no_opt)
 
@@ -27,3 +28,34 @@ def test_simple_ifelse():
 
     frame, ret = type_infer.run_analysis(simple_ifelse)
     assert ret.is_subseteq(types.Bool | types.Int | types.NoneType)
+
+
+def test_getitem_inside_loop():
+    @structural_no_opt
+    def getitem_loop():
+        ls = [1, 2, 3]
+        for i in range(2):
+            ls[i + 1]
+
+    frame, ret = type_infer.run_analysis(getitem_loop)
+
+    getitem_loop.print(analysis=frame.entries)
+
+    # NOTE: go through values, but skip method and return
+    for value in list(frame.entries.values())[1:-1]:
+        # everything except the method & return should either be int or a list[int]
+        print(value)
+        print(value.is_subseteq(types.Int | ilist.IListType[types.Int, types.Any]))
+
+    assert ret.is_subseteq(types.NoneType)
+
+    @structural_no_opt
+    def getitem_return_from_loop():
+        ls = [1, 2, 3]
+        for i in range(2):
+            return ls[i + 1]
+
+        return 1.0
+
+    frame, ret = type_infer.run_analysis(getitem_return_from_loop)
+    assert ret.is_subseteq(types.Int | types.Float)
