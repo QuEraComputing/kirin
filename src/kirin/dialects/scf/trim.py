2f668280719c50887cb2c324d4e4563dbc2eabe6
E Huang <ehua7365@users.noreply.github.com>
2025-07-29 16:16:11 -0400

    Change UnusedYield to correctly remove args
    Sometimes when there are equal args being yielded
    and equal args in the initializer with equal names,
    deleting the first variable may end up changing the
    semantics.
    This commmit corrects that behaviour.

--- src/kirin/dialects/scf/trim.py
+++ src/kirin/dialects/scf/trim.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+
 from kirin import ir
 from kirin.rewrite.abc import RewriteRule, RewriteResult
 
@@ -21,6 +23,32 @@ class UnusedYield(RewriteRule):
                 any_unused = True
         return any_unused, set(uses), results
 
+    def get_equal_args(self, node: For) -> Dict[int, List[int]]:
+        """Other iter_args indices that are equal to each index."""
+        equal_args: Dict[int, List[int]] = {}
+        original_iter_args = node.body.blocks[0].args[1:]
+        original_yields = [
+            block.last_stmt.args
+            for region in node.regions
+            for block in region.blocks
+            if isinstance(block.last_stmt, Yield)
+        ]
+
+        for idx_1 in range(len(node.results)):
+            equal_args[idx_1] = []
+            for idx_2 in range(len(node.results)):
+                are_equal = True
+                if original_iter_args[idx_1].name != original_iter_args[idx_2].name:
+                    are_equal = False
+                if node.initializers[idx_1] != node.initializers[idx_2]:
+                    are_equal = False
+                for values in original_yields:
+                    if values[idx_1] != values[idx_2]:
+                        are_equal = False
+                if are_equal and idx_1 != idx_2:
+                    equal_args[idx_1].append(idx_2)
+        return equal_args
+
     def rewrite_Statement(self, node: ir.Statement) -> RewriteResult:
         if not isinstance(node, (For, IfElse)):
             return RewriteResult()
@@ -29,7 +57,19 @@ class UnusedYield(RewriteRule):
         if not any_unused:
             return RewriteResult()
 
+        all_idx = list(range(len(node.results)))
+        idx_drop = [idx for idx in all_idx if idx not in uses]
+
+        equal_args: Dict[int, List[int]] = {}
+        if isinstance(node, For):
+            equal_args = self.get_equal_args(node)
+            equal_args = {
+                idx: [other_idx for other_idx in others if other_idx not in idx_drop]
+                for idx, others in equal_args.items()
+            }
+
         node._results = results
+
         for region in node.regions:
             for block in region.blocks:
                 if not isinstance(block.last_stmt, Yield):
@@ -38,9 +78,23 @@ class UnusedYield(RewriteRule):
                 block.last_stmt.args = [block.last_stmt.args[idx] for idx in uses]
 
         if isinstance(node, For):
-            for idx, block_arg in enumerate(node.body.blocks[0].args[1:]):
-                if idx not in uses:
-                    block_arg.replace_by(node.initializers[idx])
-                    block_arg.delete()
-            node.initializers = tuple(node.initializers[idx] for idx in uses)
+            idx_arg_drop = set()
+            iter_args = node.body.blocks[0].args[1:]
+            for idx in idx_drop:
+                if len(iter_args[idx].uses) == 0:
+                    idx_arg_drop.add(idx)
+                elif len(equal_args[idx]) == 0:
+                    idx_arg_drop.add(idx)
+                else:
+                    for equal_idx in equal_args[idx]:
+                        if len(iter_args[equal_idx].uses) == 0:
+                            idx_arg_drop.add(equal_idx)
+                            break
+            idx_arg_keep = sorted(set(all_idx) - idx_arg_drop)
+
+            for idx in sorted(idx_arg_drop):
+                block_arg = iter_args[idx]
+                block_arg.replace_by(node.initializers[idx])
+                block_arg.delete()
+            node.initializers = tuple(node.initializers[idx] for idx in idx_arg_keep)
         return RewriteResult(has_done_something=True)
