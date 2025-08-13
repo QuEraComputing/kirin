c55197939a134a140f586efcb902fb5023fcfe6f
Phillip Weinberg <weinbe58@gmail.com>
2025-08-13 11:32:43 -0400

    adding code to not repead a method name in the call graph

--- src/kirin/analysis/callgraph.py
+++ src/kirin/analysis/callgraph.py
@@ -22,22 +22,44 @@ class CallGraph(Printable):
     """Mapping from symbol names to methods."""
     backedges: dict[str, set[str]] = field(default_factory=dict)
     """Mapping from symbol names to backedges."""
+    name_counts: dict[str, int] = field(default_factory=dict)
+    """Mapping from symbol names to counts of how many times they have been used."""
+    inv_defs: dict[ir.Method, str] = field(default_factory=dict)
+    """Mapping from symbol names to methods, used for inverse lookups."""
 
     def __init__(self, mt: ir.Method):
         self.defs = {}
         self.backedges = {}
-        self.__build(mt)
+        self.name_counts = {}
+        self.inv_defs = {}
+        self.__build(mt, set([]))
 
-    def __build(self, mt: ir.Method):
-        if mt.sym_name in self.defs:
-            # Already processed this method
-            return
-        self.defs[mt.sym_name] = mt
+    def __get_name(self, mt: ir.Method) -> str:
+        """Get the name of the method, accounting for overlapping symbol names."""
+        if mt in self.inv_defs:
+            return self.inv_defs[mt]
+        else:
+            count = self.name_counts.setdefault(sym_name := mt.sym_name, 0)
+            if count > 0:  # this is needed to avoid breaking the previous logic
+                sym_name = f"{mt.sym_name}_{count + 1}"
+                self.name_counts[mt.sym_name] += 1
+
+            self.inv_defs[mt] = sym_name
+            self.defs[sym_name] = mt
+            return sym_name
+
+    def __build(self, mt: ir.Method, visited: set[str]):
+        """Build the call graph for the given method."""
+        sym_name = self.__get_name(mt)
+
         for stmt in mt.callable_region.walk():
             if isinstance(stmt, func.Invoke):
-                backedges = self.backedges.setdefault(stmt.callee.sym_name, set())
-                backedges.add(mt.sym_name)
-                self.__build(stmt.callee)
+                callee_sym_name = self.__get_name(stmt.callee)
+                backedges = self.backedges.setdefault(callee_sym_name, set())
+                backedges.add(sym_name)
+                if callee_sym_name not in visited:
+                    visited.add(callee_sym_name)
+                    self.__build(stmt.callee, visited)
 
     def get_neighbors(self, node: str) -> Iterable[str]:
         """Get the neighbors of a node in the call graph."""
