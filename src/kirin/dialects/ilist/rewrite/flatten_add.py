8599a793b622637b10ddeea3688f2e8d559827b3
Phillip Weinberg <weinbe58@gmail.com>
2025-09-24 20:40:22 -0400

    Adding rewrite to flatten sum of two ilist.New statements

---
+++ src/kirin/dialects/ilist/rewrite/flatten_add.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+from kirin import ir
+from kirin.dialects import py, ilist
+from kirin.rewrite.abc import RewriteRule, RewriteResult
+
+
+@dataclass
+class FlattenAddOpIList(RewriteRule):
+
+    def rewrite_Statement(self, node: ir.Statement) -> RewriteResult:
+        if (
+            not isinstance(node, py.binop.Add)
+            or not isinstance(lhs := node.lhs.owner, ilist.New)
+            or not isinstance(rhs := node.rhs.owner, ilist.New)
+        ):
+            return RewriteResult()
+
+        node.replace_by(ilist.New(values=lhs.values + rhs.values))
+
+        return RewriteResult(has_done_something=True)
