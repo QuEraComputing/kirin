df17e51474aa047f81bbb5c2d6959d723abea921
Neelay Fruitwala <neelayfruitwala@Neelays-MacBook-Pro.local>
2025-09-29 11:42:07 -0400

    add pos arg test

--- test/dialects/test_aggressive_inline.py
+++ test/dialects/test_aggressive_inline.py
@@ -17,9 +17,6 @@ def test_aggressive_inline():
     main_aggressive = main_aggressive.similar()
     aggressive.Fold(main_aggressive.dialects).fixpoint(main_aggressive)
 
-    main_aggressive.print()
-
-    print(main_aggressive(1))
     assert main_aggressive(1) == -1
 
 
@@ -33,9 +30,25 @@ def test_aggressive_inline_noargs():
     def main_aggressive():
         return foo(arg1=2, arg0=1)
 
-    main_aggressive.print()
-    print(main_aggressive())
     assert main_aggressive() == -1
+
+
+def test_aggressive_inline_pos_args():
+
+    @basic(aggressive=False)
+    def foo(arg0, arg1):
+        return arg0 - arg1
+
+    @basic(aggressive=True)
+    def main_aggressive(arg0):
+        return foo(arg0, 2)
+
+    main_aggressive.print()
+
+    main_aggressive = main_aggressive.similar()
+    aggressive.Fold(main_aggressive.dialects).fixpoint(main_aggressive)
+
+    assert main_aggressive(1) == -1
 
 
 #
