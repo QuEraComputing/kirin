739bd3beb90ed11f5d41e0c0f66090668c293346
Dennis Liew <dennisliew11@hotmail.com>
2025-10-07 10:10:12 -0400

    fix path

--- src/kirin/serialization/__init__.py
+++ src/kirin/serialization/__init__.py
@@ -1,8 +1,14 @@
-from .base.context import SerializationContext as SerializationContext
-from .jsonserializer import JSONSerializer as JSONSerializer
-from .base.serializer import Serializer as Serializer
-from .base.deserializer import Deserializer as Deserializer
-from .base.serializable import Serializable as Serializable
-from .base.deserializable import Deserializable as Deserializable
-from .base.serializationunit import SerializationUnit as SerializationUnit
-from .base.serializationmodule import SerializationModule as SerializationModule
+from kirin.serialization.base.context import (
+    SerializationContext as SerializationContext,
+)
+from kirin.serialization.jsonserializer import JSONSerializer as JSONSerializer
+from kirin.serialization.base.serializer import Serializer as Serializer
+from kirin.serialization.base.deserializer import Deserializer as Deserializer
+from kirin.serialization.base.serializable import Serializable as Serializable
+from kirin.serialization.base.deserializable import Deserializable as Deserializable
+from kirin.serialization.base.serializationunit import (
+    SerializationUnit as SerializationUnit,
+)
+from kirin.serialization.base.serializationmodule import (
+    SerializationModule as SerializationModule,
+)
