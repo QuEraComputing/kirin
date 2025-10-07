f15517129de853092b986bf8f2ec23cc70192727
Dennis Liew <dennisliew11@hotmail.com>
2025-10-07 10:00:39 -0400

    update serialization __init__.py

--- src/kirin/serialization/__init__.py
+++ src/kirin/serialization/__init__.py
@@ -1,3 +1,8 @@
+from .base.context import SerializationContext as SerializationContext
 from .jsonserializer import JSONSerializer as JSONSerializer
 from .base.serializer import Serializer as Serializer
 from .base.deserializer import Deserializer as Deserializer
+from .base.serializable import Serializable as Serializable
+from .base.deserializable import Deserializable as Deserializable
+from .base.serializationunit import SerializationUnit as SerializationUnit
+from .base.serializationmodule import SerializationModule as SerializationModule
