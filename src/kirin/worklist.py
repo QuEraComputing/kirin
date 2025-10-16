0cc025869595647d758fe531edba42868616147c
Casey Duckering <cduck@users.noreply.github.com>
2025-10-16 14:45:53 -0400

    Make WorkList actually O(1), not secretly O(n) (#534)
    The docstring was a lie.
    > The worklist is a stack that allows for O(1) removal of elements from
    the stack.
    
    When running bloqade-circuit and other passes my large kernels, this PR
    speeds up my program by 60% from 1120s (659s of `WorkList.pop()`) to
    453s (11s of `WorkList.pop()`)! DM me for the full profile results.
    
    Tests run on kirin v0.17.28.  Please backport to 0.17.

--- src/kirin/worklist.py
+++ src/kirin/worklist.py
@@ -1,30 +1,39 @@
 from __future__ import annotations
 
+from queue import SimpleQueue
 from typing import Generic, TypeVar, Iterable
-from dataclasses import field, dataclass
 
 ElemType = TypeVar("ElemType")
 
 
-@dataclass
-class WorkList(Generic[ElemType]):
+class WorkList(SimpleQueue, Generic[ElemType]):
     """The worklist data structure.
 
     The worklist is a stack that allows for O(1) removal of elements from the stack.
     """
 
-    _stack: list[ElemType] = field(default_factory=list, init=False)
+    def __len__(self) -> int:
+        return self.qsize()
+
+    def __bool__(self) -> bool:
+        return not self.empty()
 
     def is_empty(self) -> bool:
-        return len(self._stack) == 0
+        return self.empty()
 
     def append(self, item: ElemType) -> None:
-        self._stack.append(item)
+        self.put_nowait(item)
 
     def extend(self, items: Iterable[ElemType]) -> None:
-        self._stack.extend(items)
+        for item in items:
+            self.put_nowait(item)
 
     def pop(self) -> ElemType | None:
-        if self._stack:
-            return self._stack.pop(0)
-        return None
+        if self.empty():
+            return None
+        return self.get_nowait()
+
+
+# Remove one function call from critical speed bottleneck
+WorkList.is_empty = WorkList.empty
+WorkList.append = WorkList.put_nowait
