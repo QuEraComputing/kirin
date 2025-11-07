a9d3d6154908780bf41d39ff19d70802e2c25df2
Roger-luo <rogerluo.rl18@gmail.com>
2025-11-07 11:20:48 -0500

    cleanup main package

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod comptime;
-pub mod context;
-/// built-in dialects, but nothing special about them compared to user-defined ones
-/// they are just provided for convenience
-pub mod dialects;
-pub mod ir;
-pub mod language;
-pub mod lattice;
